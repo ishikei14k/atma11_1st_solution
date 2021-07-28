91d29eb83f3e5470f82470f0434ad0fc75a90c61
ishikei14k <kei.amnt@gmail.com>
2021-07-29 01:01:07 +0900

    delete unneeded training commands.(dino/008.sh)

--- dino/008.sh
+++ dino/008.sh
@@ -1,4 +1,1 @@
 python -m torch.distributed.launch --nproc_per_node=2 main_dino.py --num_workers 2 --arch resnet18d --epochs 300 --data_path ../input/photos_ssl --output_dir ./output/008
-
-cd ../src
-bash train_1.sh
